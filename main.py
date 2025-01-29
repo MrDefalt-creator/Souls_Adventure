a443c84fdbdec115bc3676421e0ed78f01ab4743
AlexKS <alex16abc@yandex.ru>
2025-01-29 09:36:39 +0300

    College Start

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ import pygame
 # Import pygame.locals for easier access to key coordinates
 # Updated to conform to flake8 and black standards
 
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
-
 # Initialize pygame
 pygame.init()
 
@@ -31,12 +21,7 @@ running = True
 while running:
     # Look at every event in the queue
     for event in pygame.event.get():
-        # Did the user hit a key?
-        if event.type == KEYDOWN:
-            # Was it the Escape key? If so, stop the loop.
-            if event.key == K_ESCAPE:
-                running = False
-
-        # Did the user click the window close button? If so, stop the loop.
-        elif event.type == QUIT:
+        if event.type == pygame.QUIT:
             running = False
+
+pygame.quit()
