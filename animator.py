f49be1ab3fb9d699ff28c31e53c119cdea50f824
Ivan <defalt06@mail.ru>
2025-01-31 16:28:18 +0300

    ANIMATIONS!!!!!!!!!!!!!!

--- animator.py
+++ animator.py
@@ -17,12 +17,11 @@ AnimFrames = {
     "Warrior": {
         "Idle": [(0, 0), (1, 0), (2, 0), (3, 0)],
         "Walk": [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1)],
-        "Attack": [
-            [(2, 6), (3, 6), (4, 6), (5, 6), (6, 6)],
-            [(0, 7), (1, 7), (2, 7), (3, 7)],
-            [(4, 7), (5, 7), (6, 7), (0, 8), (1, 8), (2, 8)]
-        ],
-        "Jump": [(2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (0, 3), (1, 3), (2, 3)],
+        "Attack1": [(2, 6), (3, 6), (4, 6), (5, 6), (6, 6)],
+        "Attack2": [(0, 7), (1, 7), (2, 7), (3, 7)],
+        "Attack3": [(4, 7), (5, 7), (6, 7), (0, 8), (1, 8), (2, 8)],
+        "Jump": [(2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (0, 3), (4, 2), (5, 2), (6, 2), (0, 3)],
+        "Fall": [(1, 3), (2, 3)]
 
     },
     "Witch": {
@@ -30,17 +29,23 @@ AnimFrames = {
     }
 }
 
+def getOppositeDirection(string):
+    return "Right" if string == "Left" else "Left"
 
 class Animation(sprite.Sprite):
-    def __init__(self, animName, charType, facing, isLooped, doesLinger):
-        self.isPlaying = "False"
+    def __init__(self, target, animName, charType, facing, isLooped, doesLinger, frameTime):
+        self.isPlaying = False
         self.animName = animName
         self.charType = charType
         self.facing = facing
         self.isLooped = isLooped
         self.doesLinger = doesLinger
+        self.target = target
+        self.lastFrame = 0
+        self.lastTick = time.get_ticks()
+        self.frameTime = frameTime
 
-        actual_frames = []
+        self.actual_frames = []
         Anim = AnimFrames[self.charType][self.animName]
 
         flipped = facing == "Left"
@@ -48,10 +53,31 @@ class Animation(sprite.Sprite):
         for frame in Anim:
             newframe = transform.scale(get_sprite(sheets[charType], frame[0], frame[1], sprite_params[charType][0], sprite_params[charType][1], flipped), (150, 110))
             newframe.set_colorkey((0, 0, 0))
-            actual_frames.append(newframe)
+            self.actual_frames.append(newframe)
+        
+        self.target.Animations[self.facing][self.animName] = self
 
     def play(self):
         self.isPlaying = True
+        oppositeAnim = self.target.Animations[getOppositeDirection(self.facing)][self.animName]
+
+        if oppositeAnim.isPlaying:
+            self.lastFrame = oppositeAnim.lastFrame
+            self.lastTick = oppositeAnim.lastTick
+            oppositeAnim.isPlaying = False
+
+        self.target.image = self.actual_frames[self.lastFrame]
+
+        if time.get_ticks() - self.lastTick >= self.frameTime:
+            self.lastFrame += 1
+            self.lastTick = time.get_ticks()
+
+        if self.lastFrame >= len(self.actual_frames):
+            if self.isLooped:
+                self.lastFrame = 0
+            else:
+                self.stop()
 
     def stop(self):
         self.isPlaying = False
+        self.lastFrame = 0
