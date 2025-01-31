10ae134d6d0a637f027aff498adf9c30cc4bf56e
Ivan <defalt06@mail.ru>
2025-01-31 15:47:51 +0300

    1v level generation

--- main.py
+++ main.py
@@ -3,10 +3,65 @@ import pygame
 from pygame import *
 from mainclasses import *
 from menu import *
+import random
 
 # Initialize pygame
 pygame.init()
 
+
+def generate_level(width, height):
+    level = []
+    for _ in range(height):
+        level.append([' '] * width)  # Пустые места пробелами
+
+    # Создаем землю внизу (платформа с краями)
+    if width >= 2:
+        ground_row = ['4'] + ['2'] * (width - 2) + ['5']
+    else:
+        ground_row = ['4'] * width
+    level[-1] = ground_row
+
+    current_x = 5
+    current_y = height - 5  # Начальная позиция над землей
+
+    # Генерация основных платформ
+    while current_x < width - 5:
+        platform_length = random.randint(2, 5)
+        end_x = current_x + platform_length
+        if end_x >= width:
+            end_x = width - 1
+            platform_length = end_x - current_x
+
+        # Левый край платформы
+        if current_x < width:
+            level[current_y][current_x] = '4'
+        # Середина
+        for x in range(current_x + 1, end_x):
+            if x < width:
+                level[current_y][x] = '2'
+        # Правый край
+        if end_x < width:
+            level[current_y][end_x] = '5'
+
+        # Переход к следующей платформе
+        current_x += platform_length + random.randint(2, 4)
+        # Изменение высоты
+        delta_y = random.randint(-2, 2)
+        current_y = max(3, min(current_y + delta_y, height - 2))
+
+    # Добавляем случайные небольшие платформы
+    for _ in range(20):
+        plat_x = random.randint(0, width - 3)
+        plat_y = random.randint(3, height - 4)
+        if (plat_x + 2 < width and
+                level[plat_y][plat_x] == ' ' and
+                level[plat_y][plat_x + 1] == ' ' and
+                level[plat_y][plat_x + 2] == ' '):
+            level[plat_y][plat_x] = '4'
+            level[plat_y][plat_x + 1] = '2'
+            level[plat_y][plat_x + 2] = '5'
+
+    return [''.join(row) for row in level]
 # Define constants for the screen width and height
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
@@ -34,21 +89,10 @@ entities = pygame.sprite.Group() # Все объекты
 platforms = [] # то, во что мы будем врезаться или опираться
 entities.add()
 
-level = [
-       "4",
-       "4",
-       "4",
-       "4",
-       "4",
-       "4",
-       "4",
-       "4",
-       "4                6",
-       "4",
-       "4          7",
-       "4          8",
-       "4222222222222222222222223",
-       "4000000000000000000000005"]
+# Генерируем уровень
+level_width = 100  # Ширина уровня в блоках
+level_height = 14  # Высота уровня в блоках (как в оригинале)
+level = generate_level(level_width, level_height)
 
 timer = pygame.time.Clock()
 x=y=0 # координатыz
